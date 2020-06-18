8f7465adc3e890723b59f5a2336773277a195caa
W. Guilherme <wguilhermecriativo@gmail.com>
2020-06-18 16:26:25 -0300

    [Routes] Routes tests

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+//rotas
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/cursos/curso3", (req, res) => {
+  res.send("Curso 3");
+});
+
+app.get("/reduced", (req, res) => res.send("reduced method"));
+
 app.listen(4000, function (err) {
   if (err) {
     console.log("Ocorreu um erro", err);
