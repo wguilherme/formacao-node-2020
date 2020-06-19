7a7f9f2d4fc13dcbe7b18d2607c95bf96e812bea
W. Guilherme <wguilhermecriativo@gmail.com>
2020-06-18 23:57:11 -0300

    [Update] Question page with Bootstrap container, form control and button classes

--- projetos/perguntas-e-respostas/index.js
+++ projetos/perguntas-e-respostas/index.js
@@ -6,22 +6,11 @@ app.set("view engine", "ejs");
 // static folder
 app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-  const produtos = [
-    { name: "Mouse Razer", price: 497, category: "Informática" },
-    { name: "Teclado Logitech", price: 800, category: "Informática" },
-    { name: "Iphone X", price: 6000, category: "Celular" },
-  ];
-
-  const nome = "Withney";
-  const lang = "Javascript";
-  res.render("index", {
-    nome: nome, //variável
-    lang: lang, //variável
-    empresa: "Teste", //definido na hora
-    valor: 5000, // definido na hora
-    produtos, // item com mesmo nome da variável não é necessário indicar
-  });
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
 });
 
 app.listen(4000, () => {
