f816aac1de416b070780d942f813a67d1ab6a2c2
W. Guilherme <ravok123@gmail.com>
2020-09-15 15:45:08 -0300

    [Update] Setup socket.io

---
+++ projetos/socketio/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+// instância da aplicação
+var app = express();
+// socket.io precisa do servidor http nativo do node
+// mas pode passar a instância do app (exopress)
+var http = require('http').createServer(app)
+// agora sim instanciamos o socket.io
+var io = require('socket.io')(http)
+
+// sempre que um cliente se conecta
+// param socket é o cliente
+io.on("connection", (socket) => {
+   console.log(socket)
+   // ID do client
+   console.log(socket.id)
+
+})
+
+app.set('view engine', 'ejs');
+
+
+app.get("/", (req, res) => {
+   res.render("index");
+   console.log('rodou')
+})
+
+
+// precisamos abrir o servidor nativo
+http.listen(3000, () => {
+   console.log('app rodando')
+})
